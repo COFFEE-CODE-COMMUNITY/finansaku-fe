c1e2412dbc76378f0418e070267c50444c6a2ceb
rafi tanujaya <rafibussines@gmail.com>
2025-10-30 23:06:03 +0700

    refactor config base url

--- src/config/script.js
+++ src/config/script.js
@@ -1,10 +1,5 @@
 const config = {
-    BASE_URL_REGISTER: import.meta.env.VITE_BASE_URL_REGISTER,
-    BASE_URL_LOGIN: import.meta.env.VITE_BASE_URL_LOGIN,
+    BASE_URL: import.meta.env.VITE_BASE_URL,
 }
 
 export default config
-
-// VITE_API_BASE_URL=https://charolette-unmasterful-unpliably.ngrok-free.dev/api/v1
-// VITE_API_BASE_URL_REGISTER=https://charolette-unmasterful-unpliably.ngrok-free.dev/api/v1
-// VITE_API_BASE_URL_LOGIN=https://charolette-unmasterful-unpliably.ngrok-free.dev
