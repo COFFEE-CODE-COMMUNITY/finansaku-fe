6346f9647e92a475be6cb7ed19d426c19b304c44
rafi tanujaya <rafibussines@gmail.com>
2025-11-15 20:02:17 +0700

    refactor: adding vite config react

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
-  import React from 'react'
+import { StrictMode } from 'react'
   import { createRoot } from 'react-dom/client'
-  import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom' 
+  import { BrowserRouter, Routes, Route } from 'react-router-dom' 
   import './index.css'
   import {UserProvider} from './context/userProvider.jsx'
   import Login from './pages/auth/loginPage'
@@ -31,44 +31,47 @@
   import ChangeEmail from './components/changeEmail.jsx'
   import DefDashboard from './components/defDashboard.jsx'
 
-  createRoot(document.getElementById('root')).render(
-    <UserProvider>
-    <ReminderProvider>
-      <BrowserRouter> 
-          <Toaster position="top-center" reverseOrder={false} /> 
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+      <UserProvider>
+      <ReminderProvider>
+        <BrowserRouter> 
+            <Toaster position="top-center" reverseOrder={false} /> 
 
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-              <Route path="/login" element={<Login />} />
-              <Route path='/signUp' element={<SignUP/>}/> 
-              <Route path="/readArticle" element={<ReadArticle />} />
-              <Route path="/article" element={<ArticlePage />} />
-              <Route path="/waitPage" element={<WaitPage />} />
-              <Route path="/forgotPassword" element={<ForgotPassword />} />
-              <Route path="/reset-password" element={<PassChangePage />} />
-              <Route path="/privacy-police" element={<PrivacyPolicy />} />           
-                       
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+                <Route path="/login" element={<Login />} />
+                <Route path='/signUp' element={<SignUP/>}/> 
+                <Route path="/readArticle" element={<ReadArticle />} />
+                <Route path="/article" element={<ArticlePage />} />
+                <Route path="/waitPage" element={<WaitPage />} />
+                <Route path="/forgotPassword" element={<ForgotPassword />} />
+                <Route path="/reset-password" element={<PassChangePage />} />
+                <Route path="/privacy-police" element={<PrivacyPolicy />} />           
+                        
 
-              <Route path="/oauth-success" element={<OauthSuccess/>} />
-              <Route element={<ProtectedRoute><MainLayout/></ProtectedRoute>}>
-                <Route path="/dashboard" element={<DashboardSet />} />
-                <Route path="/survey" element={<Survey />} />
-                <Route path="/calendar" element={<Calendar />} />
-                <Route path="/history" element={<History />} />
-                <Route path="/notification" element={<Notification />} />
-                <Route path="/default-dashbaord" element={<DefDashboard />} />  
-              </Route>
-
-               <Route element={<SettingLayout />}>
-                  <Route path="/setting" element={<Setting />} />
-                  <Route path="/account-information" element={<AccountInfo />} />
-                  <Route path="/change-password" element={<ChangePassSetting />} />
-                  <Route path="/change-email" element={<ChangeEmail />} />
+                <Route path="/oauth-success" element={<OauthSuccess/>} />
+                <Route element={<ProtectedRoute><MainLayout/></ProtectedRoute>}>
+                  <Route path="/dashboard" element={<DashboardSet />} />
+                  <Route path="/survey" element={<Survey />} />
+                  <Route path="/calendar" element={<Calendar />} />
+                  <Route path="/history" element={<History />} />
+                  <Route path="/notification" element={<Notification />} />
+                  <Route path="/default-dashbaord" element={<DefDashboard />} />  
                 </Route>
 
-          </Routes>
+                <Route element={<SettingLayout />}>
+                    <Route path="/setting" element={<Setting />} />
+                    <Route path="/account-information" element={<AccountInfo />} />
+                    <Route path="/change-password" element={<ChangePassSetting />} />
+                    <Route path="/change-email" element={<ChangeEmail />} />
+                  </Route>
 
-      </BrowserRouter>
-    </ReminderProvider>
-    </UserProvider>
+            </Routes>
+
+        </BrowserRouter>
+      </ReminderProvider>
+      </UserProvider>
+
+    </StrictMode>
   )
